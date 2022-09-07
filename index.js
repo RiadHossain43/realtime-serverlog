b5df4bfff0c6d657531d44232dcecbbfc4cb0c3e
RiadHossain43 <riadhossain7464@gmail.com>
2022-09-07 19:58:11 +0600

    fix: stream limiteation problem fixed

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-require('dotenv').config()
-let socketConnection = require('./services/webSocketService')
+require("dotenv").config();
+let socketConnection = require("./services/webSocketService");
 function _subscribe(url) {
-    socketConnection.init(url, { query: {} })
-    socketConnection.getSocket().on('disconnect', (reason) => {
-        console.log("Socket disconnected", reason)
-    })
-    socketConnection.getSocket().on('connect', () => {
-        console.log("Socket connected to", url)
-        socketConnection.getSocket().emit('logger-subscription', { logKey: "Some key" })
-        socketConnection.getSocket().on('logger-subscription-response', _data => {
-            console.log("Subscription details", _data)
-        })
-        socketConnection.getSocket().on('log-stream', _data => {
-            _data && console.log(..._data)
-        })
-    })
+  socketConnection.init(url, { query: {} });
+  socketConnection.getSocket().on("disconnect", (reason) => {
+    console.log("Socket disconnected", reason);
+  });
+  socketConnection.getSocket().on("connect", () => {
+    console.log("Socket connected to", url);
+    socketConnection
+      .getSocket()
+      .emit("logger-subscription", { logKey: "Some key" });
+    socketConnection.getSocket().on("logger-subscription-response", (_data) => {
+      console.log("Subscription details", _data);
+    });
+    if (!socketConnection?.getSocket()?._callbacks["$" + "log-stream"])
+      socketConnection.getSocket().on("log-stream", (_data) => {
+        _data && console.log(..._data);
+      });
+  });
 }
-module.exports = _subscribe
+module.exports = _subscribe;
